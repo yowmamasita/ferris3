4d0188d7899c46463602ae5d2ecac1940f10eafd
Jon Wayne Parrott <jjramone13@gmail.com>
2014-07-11 16:49:15 -0400

    Abstracting out the implementation from the wrappers

--- lib/ferris3/hvild.py
+++ lib/ferris3/hvild.py
@@ -6,98 +6,133 @@ from endpoints import NotFoundException
 from protorpc import message_types
 from ferris3.utils.messages import list_message
 
+#
+# Method implementations.
+# Bare-bones, can be re-used in other methods and such.
+#
 
-def list(Model, Message=None, ListMessage=None):
+def list_impl(ListMessage, query):
+    return ApiChain(query) \
+        .messages.serialize_list(ListMessage) \
+        .value()
+
+
+def paginated_list_impl(ListMessage, query, limit, page_token):
+    return ApiChain(query) \
+        .ndb.paginate(limit=limit, page_token=page_token) \
+        .messages.serialize_list(ListMessage) \
+        .value()
+
+
+def get_impl(Model, Message, item_key):
+    return ApiChain(item_key) \
+        .ndb.get() \
+        .raise_if(None, NotFoundException()) \
+        .ndb.check_kind(Model) \
+        .messages.serialize(Message) \
+        .value()
+
+
+def delete_impl(Model, item_key):
+    return ApiChain(item_key) \
+        .ndb.key() \
+        .ndb.check_kind(Model) \
+        .ndb.delete() \
+        .value()
+
+
+def update_impl(Model, Message, item_key, request):
+    item = ApiChain(item_key) \
+        .ndb.get() \
+        .raise_if(None, NotFoundException()) \
+        .ndb.check_kind(Model) \
+        .value()
+
+    return ApiChain(request) \
+        .messages.deserialize(item) \
+        .ndb.put() \
+        .messages.serialize(Message) \
+        .value()
+
+
+def insert_impl(Model, Message, request):
+    return ApiChain(request) \
+        .messages.deserialize(Model) \
+        .ndb.put() \
+        .messages.serialize(Message) \
+        .value()
+
+#
+# Full method wrappers.
+# Can be used as endpoint methods directly.
+#
+
+def list(Model, Message=None, ListMessage=None, name='list'):
     if not Message:
         Message = protopigeon.model_message(Model)
 
     if not ListMessage:
         ListMessage = list_message(Message)
 
-    @auto_method(returns=ListMessage, name='list')
+    @auto_method(returns=ListMessage, name=name)
     def inner(self, request):
-        return ApiChain(Model.query()) \
-            .messages.serialize_list(ListMessage) \
-            .value()
+        return list_impl(ListMessage, Model.query())
 
     return inner
 
 
-def paginated_list(Model, Message=None, ListMessage=None, limit=50, **kwargs):
+def paginated_list(Model, Message=None, ListMessage=None, limit=50, name='list'):
     if not Message:
         Message = protopigeon.model_message(Model)
 
     if not ListMessage:
         ListMessage = list_message(Message)
 
-    @auto_method(returns=ListMessage, name=kwargs.get('name', 'list'))
+    @auto_method(returns=ListMessage, name=name)
     def inner(self, request, page_token=(str, '')):
-        return ApiChain(Model.query()) \
-            .ndb.paginate(limit=limit, page_token=page_token) \
-            .messages.serialize_list(ListMessage) \
-            .value()
+        return paginated_list_impl(ListMessage, Model.query(), limit, page_token)
 
     return inner
 
-def get(Model, Message=None):
+
+def get(Model, Message=None, name='get'):
     if not Message:
         Message = protopigeon.model_message(Model)
 
-    @auto_method(returns=Message, name='get')
+    @auto_method(returns=Message, name=name)
     def inner(self, request, item_key=(str,)):
-        return ApiChain(item_key) \
-            .ndb.get() \
-            .raise_if(None, NotFoundException()) \
-            .messages.serialize(Message) \
-            .value()
+        return get_impl(Model, Message, item_key)
 
     return inner
 
 
-def delete(Model):
+def delete(Model, name='delete'):
 
-    @auto_method(name='delete', http_method='DELETE')
+    @auto_method(name=name, http_method='DELETE')
     def inner(self, request, item_key=(str,)):
-
-        ApiChain(item_key) \
-            .ndb.key() \
-            .ndb.check_kind(Model) \
-            .ndb.delete()
+        delete_impl(Model, item_key)
+        return None
 
     return inner
 
 
-def insert(Model, Message=None):
+def insert(Model, Message=None, name='insert'):
     if not Message:
         Message = protopigeon.model_message(Model)
 
-    @auto_method(returns=Message, name='insert', http_method='POST')
+    @auto_method(returns=Message, name=name, http_method='POST')
     def inner(self, request=(Message,)):
-        return ApiChain(request) \
-            .messages.deserialize(Model) \
-            .ndb.put() \
-            .messages.serialize(Message) \
-            .value()
+        return insert_impl(Model, Message, request)
 
     return inner
 
 
-def update(Model, Message=None):
+def update(Model, Message=None, name='update'):
     if not Message:
         Message = protopigeon.model_message(Model)
 
-    @auto_method(returns=Message, name='update', http_method='POST')
+    @auto_method(returns=Message, name=name, http_method='POST')
     def inner(self, request=(Message,), item_key=(str,)):
-        item = ApiChain(item_key) \
-            .ndb.key() \
-            .ndb.get() \
-            .raise_if(None, NotFoundException()) \
-            .value()
-
-        return ApiChain(request) \
-            .messages.deserialize(item) \
-            .ndb.put() \
-            .messages.serialize(Message) \
-            .value()
+        return update_impl(Model, Message, item_key, request)
 
     return inner
