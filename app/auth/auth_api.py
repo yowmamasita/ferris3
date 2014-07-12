fd73d0c8783e80534ff05ac854e1399cda1ac0ff
Jon Wayne Parrott <jjramone13@gmail.com>
2014-07-12 18:34:23 -0400

    Formatting

--- app/auth/auth_api.py
+++ app/auth/auth_api.py
@@ -3,11 +3,11 @@ from protopigeon.types import UserMessage
 from ferris3 import auto_class, auto_method, Service
 
 
-@auto_class   
+@auto_class
 class AuthInfoApi(Service):
 
     @auto_method(returns=UserMessage)
-    def info(self, request):    
+    def info(self, request):
         user = endpoints.get_current_user()
         if user:
             return UserMessage(
--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from ferris3.discovery import discover_api_services
 from ferris3 import apis
 
 apis.add('default-api.yaml', default=True)
+
 API_CLASSES = discover_api_services()
 APPLICATION = endpoints.api_server(API_CLASSES)
