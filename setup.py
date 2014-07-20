b4a97ab4e6c8f87f5a3574fbac0cf619f4b5506e
Jon Wayne Parrott <jjramone13@gmail.com>
2014-07-19 21:02:07 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = '3.0.0-alpha'
+
+setup(
+    name="Ferris",
+    version=VERSION,
+    author='Jon Parrott',
+    author_email='jjramone13@gmail.com',
+    maintainer='Jon Parrott / Cloud Sherpas',
+    maintainer_email='jonathan.parrott@cloudsherpas.com',
+    description='A framework for creating APIs on Google App Engine',
+    url='https://bitbucket.org/cloudsherpas/ferris3',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    packages=['protopigeon'],
+    install_requires=[
+    ],
+)
