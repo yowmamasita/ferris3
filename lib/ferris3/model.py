069b280c7cf5515f0ef4a6624a4da8bda74f9d8d
Jon Wayne Parrott <jjramone13@gmail.com>
2014-07-19 20:29:01 -0400

    Removing some unused model functionality

--- lib/ferris3/model.py
+++ lib/ferris3/model.py
@@ -8,9 +8,7 @@ import types
 
 class ModelMeta(ndb.model.MetaModel):
     """
-    Augments Models by adding the class methods find_all_by_x
-    and find_by_x that are proxies for find_all_by_properties and
-    find_by_properties respectively.
+    Ensures behaviors are constructed.
     """
     def __init__(cls, name, bases, dct):
         super(ModelMeta, cls).__init__(name, bases, dct)
@@ -22,78 +20,15 @@ class ModelMeta(ndb.model.MetaModel):
         # Behaviors
         setattr(cls, 'behaviors', [x(cls) for x in cls.Meta.behaviors])
 
-        # Inject finder methods
-        ModelMeta._inject_find_methods(cls, name, bases, dct)
-
-    @staticmethod
-    def _inject_find_methods(cls, name, bases, dct):
-        # find_by_x and find_all_by_x
-        for prop_name, property in cls._properties.items():
-            find_all_name = 'find_all_by_' + prop_name
-
-            def bind_all(name):
-                def find_all(cls, value):
-                    args = {}
-                    args[name] = value
-                    return cls.find_all_by_properties(**args)
-                return types.MethodType(find_all, cls)
-
-            if not find_all_name in dct:
-                setattr(cls, find_all_name, bind_all(prop_name))
-
-            find_one_name = 'find_by_' + prop_name
-
-            def bind_one(name):
-                def find_one(cls, value):
-                    args = {}
-                    args[name] = value
-                    return cls.find_by_properties(**args)
-                return types.MethodType(find_one, cls)
-
-            if not find_one_name in dct:
-                setattr(cls, find_one_name, bind_one(prop_name))
-
 
 class Model(ndb.Model):
     """
-    Base class that augments ndb Models by adding easier find methods and callbacks.
+    Model that has behaviors attached to it.
     """
     __metaclass__ = ModelMeta
 
     class Meta(object):
         behaviors = ()
-
-    @classmethod
-    def find_all_by_properties(cls, **kwargs):
-        """
-        Generates an ndb.Query with filters generated from the keyword arguments.
-
-        Example::
-
-            User.find_all_by_properties(first_name='Jon',role='Admin')
-
-        is the same as::
-
-            User.query().filter(User.first_name == 'Jon', User.role == 'Admin')
-
-        """
-        query = cls.query()
-        for name, value in kwargs.items():
-            property = cls._properties[name]
-            query = query.filter(property == value)
-        return query
-
-    @classmethod
-    def find_by_properties(cls, **kwargs):
-        """
-        Similar to find_all_by_properties, but returns either None or a single ndb.Model instance.
-
-        Example::
-
-            User.find_by_properties(first_name='Jon',role='Admin')
-
-        """
-        return cls.find_all_by_properties(**kwargs).get()
 
     def before_put(self):
         """
@@ -186,25 +121,6 @@ class Model(ndb.Model):
         res = future.get_result()
         cls._invoke_behaviors('after_get', res)
         return cls.after_get(key, res)
-
-    def __unicode__(self):
-        if hasattr(self, 'name'):
-            return self.name or super(Model, self).__str__()
-        else:
-            return super(Model, self).__str__()
-
-    def __str__(self):
-        return self.__unicode__()
-
-
-class BasicModel(Model):
-    """
-    Adds the common properties created, created_by, modified, and modified_by to :class:`Model`
-    """
-    created = ndb.DateTimeProperty(auto_now_add=True)
-    created_by = ndb.UserProperty(auto_current_user_add=True)
-    modified = ndb.DateTimeProperty(auto_now=True)
-    modified_by = ndb.UserProperty(auto_current_user=True)
 
 
 class Behavior(object):
