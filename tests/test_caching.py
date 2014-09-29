4bbb0df709a555ed247c3b0f854fcfa4028e651c
Jon Wayne Parrott <jjramone13@gmail.com>
2014-09-29 16:55:07 -0400

    Better tests for caching

--- tests/test_caching.py
+++ tests/test_caching.py
@@ -1,14 +1,15 @@
 from ferrisnose import AppEngineTest
 from google.appengine.api import memcache
-from ferris3.caching import cache, none_sentinel_string, LocalBackend, MemcacheBackend, DatastoreBackend, MemcacheCompareAndSetBackend, LayeredBackend
+from ferris3.caching import cache, cache_by_args, none_sentinel_string, LocalBackend, MemcacheBackend, DatastoreBackend, MemcacheCompareAndSetBackend, LayeredBackend, DatastoreCacheModel
 
 
 class CacheTest(AppEngineTest):
 
     def test_cache(self):
-        for backend in [LocalBackend, MemcacheBackend, DatastoreBackend, MemcacheCompareAndSetBackend, LayeredBackend(LocalBackend, MemcacheBackend)]:
+        for backend in [None, 'local', 'datastore', LocalBackend, MemcacheBackend, DatastoreBackend, MemcacheCompareAndSetBackend, LayeredBackend(LocalBackend, MemcacheBackend)]:
             memcache.flush_all()
             LocalBackend.reset()
+            map(lambda x: x.key.delete(), DatastoreCacheModel.query())
 
             mutators = [0, 0, 0]
             print 'testing %s' % backend
@@ -17,6 +18,13 @@ class CacheTest(AppEngineTest):
             def test_cached():
                 mutators[0] += 1
                 return mutators[0]
+
+            if not backend:
+                backend = MemcacheBackend
+            if backend == 'local':
+                backend = LocalBackend
+            if backend == 'datastore':
+                backend = DatastoreBackend
 
             assert test_cached() == 1
             assert test_cached() == 1
@@ -37,3 +45,31 @@ class CacheTest(AppEngineTest):
             assert test_cached_with_none() is None
             assert mutators[1] == 1
             assert backend.get('cache-test-key-none') == none_sentinel_string
+            assert test_cached_with_none.cached() is None
+
+            @cache_by_args('cache-test-args')
+            def test_cached_with_args(arg):
+                return arg
+
+            assert test_cached_with_args(1) == 1
+            assert test_cached_with_args(2) == 2
+
+            @cache_by_args('cache-test-args-method')
+            def test_cached_with_args(self, arg):
+                return arg
+
+            assert test_cached_with_args(None, 1) == 1
+            assert test_cached_with_args(None, 2) == 2
+
+    def test_expiration(self):
+        for backend in LocalBackend, DatastoreBackend:
+            backend.set('expiry', 1, ttl=-1)
+            assert not backend.get('expiry')
+
+    def test_delete(self):
+        LocalBackend.delete('non-existant')
+
+    def test_cas(self):
+        MemcacheCompareAndSetBackend.set('test', 1, 0)
+        MemcacheCompareAndSetBackend.set('test', 2, 0)
+        assert MemcacheCompareAndSetBackend.get('test') == 2
