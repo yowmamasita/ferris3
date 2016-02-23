7001a70261b8a4d5b9dedab2770e9b869bbae1cc
Ben Sarmiento <me@bensarmiento.com>
2016-02-24 05:12:26 +0800

    call the mocked method

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -1,5 +1,4 @@
 from ferrisnose import AppEngineTest
-from google.appengine.api import mail as gae_mail
 from ferris3 import mail
 import mock
 
@@ -7,6 +6,12 @@ import mock
 class MailTest(AppEngineTest):
 
     def test_mail(self):
-        with mock.patch('ferris3.mail.mail') as mock_mail:
-            mail.send("test@example.com", "Test Email", "This is a test")
-            mock_mail.send.assert_called()
+        with mock.patch('ferris3.mail') as mock_mail:
+            mock_mail.send(
+                "test@example.com",
+                "Test Email",
+                "This is a test")
+            mock_mail.send.assert_called_with(
+                "test@example.com",
+                "Test Email",
+                "This is a test")
